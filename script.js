3c2812ea7a8a4f85efdc26d4b19c11c62aadd595
Deji985 <akerejola.ayodeji@acity.edu.gh>
2025-12-14 06:48:13 +0000

    updated backend url

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const API_BASE = '/api';
+const API_BASE = 'https://campusfoodorderingsystembcknd.onrender.com';
 
 function fetchWithAuth(url, options = {}) {
     const token = localStorage.getItem('token');
